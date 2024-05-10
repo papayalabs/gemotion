450abc316f45808c88352ee8fb037732b1fc173e
BOUT Valentin <bout.valentin@gmail.com>
2024-05-10 11:01:56 +0200

    Feat: add header, footer, seo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'static/home'
+  get 'static/about'
+  get 'static/prices'
+
+
+  # Static pages
+  get 'about', to: 'static#about', as: "about"
+  get 'prices', to: 'static#prices', as: "price"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "static#home"
 end
