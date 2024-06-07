8228be98038cae83dd286db4752cbf9420bf432c
Maxime Potier <maxime.potier@gmail.com>
2024-06-07 07:14:25 +0200

    Design integration for header completed

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,30 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Montserrat", "Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "header-bg": "#201F1F",
+      },
+      spacing: {
+        "14.43px": "14.43px",
+        "16.78px": "16.78px",
+        "28.32px": "28.32px",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
