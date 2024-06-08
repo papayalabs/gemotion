a6dafd780d52990e6f176ce4b86570a9978ef706
Maxime Potier <maxime.potier@gmail.com>
2024-06-08 16:09:17 +0200

    Swiper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+
+import Swiper from "swiper/bundle";
+import "swiper/swiper-bundle.css";
+
+const { environment } = require("@rails/webpacker");
+const webpack = require("webpack");
+
+// Ajouter une configuration supplémentaire si nécessaire
+
+module.exports = environment;
