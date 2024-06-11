69acd0a004fb85aab1cdaa1b308e6931c1ca66c2
Maxime Potier <maxime.potier@gmail.com>
2024-06-11 20:21:09 +0200

    Minor updates

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,13 +9,13 @@ document.addEventListener("turbo:load", function () {
     modules: [Navigation, Pagination],
     slidesPerView: 1,
     spaceBetween: 10,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
   });
 });
