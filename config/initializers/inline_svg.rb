be0c366cf49062b9c8a769584f4ca605cdb949f0
Maxime Potier <maxime.potier@gmail.com>
2024-06-13 13:57:00 +0200

    Videos steps

---
+++ config/initializers/inline_svg.rb
@@ -0,0 +1,5 @@
+# InlineSvg.configure do |config|
+#   config.asset_finder = InlineSvg::StaticAssetFinder
+#   config.svg_not_found_css_class = 'icon-missing'
+#   config.svg_not_found_size = '32x32'
+# end
