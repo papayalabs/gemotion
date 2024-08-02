3266344dc5483c33b37c8e1d31dc1efe6abe6bc4
Sid <contact@sidali.dev>
2024-08-02 05:53:55 +0000

    feature(swiper): initialize it

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,4 +3,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "swiper", to: "https://unpkg.com/swiper/swiper-bundle.min.js"
+pin 'swiper', to: 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
