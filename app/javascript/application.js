3d986ab2d6ef1c4f51a8f26a4d5c1fb6861fdad6
Sid <contact@sidali.dev>
2024-09-09 16:05:03 +0200

    swiper

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
-import "@hotwired/turbo-rails";
-import "controllers";
-import './init_swiper'
+import '@hotwired/turbo-rails'
+import 'controllers'
+
+import Swiper from 'swiper'
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    hide: true,
+  },
+})
